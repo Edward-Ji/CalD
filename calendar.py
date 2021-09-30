5a2301fc0eb9832668c632777c48c36e2fa449bc
Edward Ji <jiziao6@gmail.com>
2021-09-30 13:50:27 +1000

    Add named pipe handling

--- calendar.py
+++ calendar.py
@@ -2,9 +2,23 @@ import os
 import sys
 
 
+# Constants for file paths
+PIPE_PATH = "/tmp/cald_pipe"
+
+
 def run():
-    # YOUR CODE HERE
-    pass
+    # Check if named pipe exists
+    if not os.path.exists(PIPE_PATH):
+        sys.stderr.write("PIPE DOESN'T EXISTS\n")
+        return
+
+    # Open named pipe in write only mode
+    pipe = open(PIPE_PATH, "w")
+    try:
+        pipe.write(sys.argv[1] + "\n")
+    except OSError:
+        sys.stdout.write("Pipe has been closed\n")
+    pipe.close()
 
 
 if __name__ == '__main__':
--- daemon.py
+++ daemon.py
@@ -1,10 +1,13 @@
-#!/bin/python
+#!/usr/bin/env python
 import signal
 import os
 import sys
 
 
-# Use this variable for your loop
+# Constants for file paths
+PIPE_PATH = "/tmp/cald_pipe"
+
+# Use this variable in your loop
 daemon_quit = False
 
 
@@ -18,9 +21,23 @@ def run():
     # Do not modify or remove this function call
     signal.signal(signal.SIGINT, quit_gracefully)
 
-    # Call your own functions from within
-    # the run() funcion
-    pass
+    # Create the named pipe if it does not exist
+    if not os.path.exists(PIPE_PATH):
+        os.mkfifo(PIPE_PATH)
+
+    # Open named pipe in readonly mode
+    pipe = open(PIPE_PATH, "r")
+
+    # Main loop guarded by quit flag
+    while not daemon_quit:
+        # Read from the named pipe
+        command = pipe.readline()
+
+        # Temporary output command
+        sys.stdout.write(command)
+
+    # Close named pipe
+    pipe.close()
 
 
 if __name__ == '__main__':
